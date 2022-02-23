1423b71ff94f43ddcf0b6738b16021612ddd2016
Chris Clement <chris.clement1995@gmail.com>
2022-02-23 17:11:00 +0000

    Updated timing framework so methods are more flexible and use parameters instead of hard coded values

--- AlgorithmicComplexityI/lib/timing_framework.rb
+++ AlgorithmicComplexityI/lib/timing_framework.rb
@@ -23,17 +23,37 @@ class TimingFramework
   def run_algo_timer(array_of_arrays)
     array_of_arrays.each do |array|
       start = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-      array.last
+      # change below what algo you want to test
+      array.sort
       finish = Process.clock_gettime(Process::CLOCK_MONOTONIC)
       time_in_millis = (finish - start) * 1000
       @algo_timer_results << { 'size': array.length, 'time': time_in_millis}
     end
   end
 
-  def runTest
-    create_sample_array(10, 20, 2)
+  def runTest(start_size, end_size, increment)
+    @algo_timer_results = []
+    create_sample_array(start_size, end_size, increment)
     run_algo_timer(@sample_array)
-    print @algo_timer_results
+  end
+
+  def run_test_and_save_to_text(repitions, start_size, end_size, increment)
+    repitions.times do 
+      runTest(start_size, end_size, increment)
+    end
+    File.write('./lib/data.txt', "Size\n", mode: 'w')
+    @algo_timer_results.each do |result|
+      File.open('./lib/data.txt', 'a') {
+      |f| f.write "#{result[:size]}\n"
+      }
+    end
+    File.write('./lib/data.txt', "Time\n", mode: 'a')
+    @algo_timer_results.each do |result|
+      File.open('./lib/data.txt', 'a') {
+      |f| f.write "#{result[:time]}\n"
+      }
+    end
+    puts "done"
   end
 
 end
