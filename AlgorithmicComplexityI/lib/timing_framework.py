42fc62d5506a3b25d8ccc152a4d7a55c6e94f5c3
Chris Clement <chris.clement1995@gmail.com>
2022-02-28 13:57:36 +0000

    Data is now plotted on a graph each time the timing_framework.py is run

--- AlgorithmicComplexityI/lib/timing_framework.py
+++ AlgorithmicComplexityI/lib/timing_framework.py
@@ -3,6 +3,10 @@
 
 import time
 from numpy import random
+import pip
+pip.main(["install","matplotlib"])
+import matplotlib.pyplot as plt
+
 
 class TimingFramework:
 
@@ -54,5 +58,13 @@ class TimingFramework:
 
 example = TimingFramework()
 
-example.run_test_and_save_to_text(10, 5000, 100000, 5000)
-print(example._algo_timer_results_size)
+example.run_test_and_save_to_text(1, 5000, 100000, 5000)
+x = example._algo_timer_results_size
+y = example._algo_timer_results_time
+print(x)
+print(y)
+plt.plot(x, y)
+plt.xlabel('Size of Array')
+plt.ylabel('Time in microseconds')
+plt.title('Algorithmic Efficiency')
+plt.show()
