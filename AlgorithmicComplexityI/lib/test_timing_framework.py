b43e281210552e17162f09fec977143f7870801f
Chris Clement <chris.clement1995@gmail.com>
2022-03-01 10:49:06 +0000

    rewritten tests using pytest

---
+++ AlgorithmicComplexityI/lib/test_timing_framework.py
@@ -0,0 +1,8 @@
+import pytest
+from timing_framework import *
+
+def test_my_reverse(monkeypatch):
+  timing_framework = TimingFramework()
+  assert isinstance(timing_framework.my_reverse([1, 2, 3]), list) == True
+  assert timing_framework.my_reverse([1, 2, 3]) == [3, 2, 1]
+
--- AlgorithmicComplexityI/lib/timing_framework.py
+++ AlgorithmicComplexityI/lib/timing_framework.py
@@ -62,14 +62,14 @@ class TimingFramework:
 
 
 
-example = TimingFramework()
+# example = TimingFramework()
 
-example.run_test_and_save_to_text(1, 5000, 100000, 5000)
-x = example._algo_timer_results_size
-y = example._algo_timer_results_time
-plt.plot(x, y)
-plt.xlabel('Size of Array')
-plt.ylabel('Time in microseconds')
-plt.title('Algorithmic Efficiency')
-plt.show()
+# example.run_test_and_save_to_text(1, 5000, 100000, 5000)
+# x = example._algo_timer_results_size
+# y = example._algo_timer_results_time
+# plt.plot(x, y)
+# plt.xlabel('Size of Array')
+# plt.ylabel('Time in microseconds')
+# plt.title('Algorithmic Efficiency')
+# plt.show()
 
